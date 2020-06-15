e01b2f1653f2419aa766c54b38c7db89549f3953
czar3k <czar3k@andraka.eu>
2020-06-16 00:12:24 +0200

    first commit

---
+++ scatter_plot.py
@@ -0,0 +1,20 @@
+import pandas as pd
+
+columns = pd.read_csv('data/avoidance_ch.csv', sep=';', nrows=0).columns
+#rows = pd.read_csv('data/avoidance_ch.csv', sep=';', nrows=0).rows
+
+f = {col : lambda x : (x.replace(",", ".")) for col in columns}
+df = pd.read_csv('data/avoidance_ch.csv', sep=';',converters=f)
+
+print(df.head(5))
+print(df['ID_ch'][0]+df['RD_ch'][1])
+#print(df['RD_ch'].dtypes)
+print(df.iat[1,1])
+
+
+print(len(df.columns))
+print(len(df))
+
+
+t = '1,4'
+#print(f(t))
