85481783e709f4cbd4d4e054551feda88a3b9c0c
root <root@N-5CG8293XK7.corpo.t-mobile.pl>
2020-06-16 18:19:45 +0200

    Two tabs in one csv file

--- scatter_plot.py
+++ scatter_plot.py
@@ -1,20 +1,38 @@
 import pandas as pd
 
-columns = pd.read_csv('data/avoidance_ch.csv', sep=';', nrows=0).columns
+file = 'data/RT_exp vs ctrl_avoidance.csv'
 
+columns = pd.read_csv(file, sep=';', nrows=0).columns
+ccc = pd.read_csv(file, sep=';', header=None)
 
 f = {col : lambda x : (x.replace(",", ".")) for col in columns}
-df = pd.read_csv('data/avoidance_ch.csv', sep=';',converters=f)
+df = pd.read_csv(file, sep=';',converters=f)
 
 print(df.head(5))
-print(df['ID_ch'][0]+df['RD_ch'][1])
+#print(df['ID_ch'][0]+df['RD_ch'][1])
 #print(df['RD_ch'].dtypes)
 print(df.iat[1,1])
 
 
 print(len(df.columns))
 print(len(df))
+print('#######  BRAKE ######\n')
 
+tabcol1 = []
+tabcol2 = []
+tabnr = 1
 
-t = '1,4'
-#print(f(t))
+for i in range(0,len(df.columns)):
+    #print(ccc.iat[0,i])
+    cn = ccc.iat[0,i]
+    if tabnr == 1 and str(cn) != 'nan':
+        tabcol1.append(cn)
+    elif tabnr == 2:
+        tabcol2.append(cn)
+    if str(cn) == 'nan':
+        tabnr += 1
+    
+
+print('tabcol1: ', tabcol1)
+
+print('tabcol2: ', tabcol2)
