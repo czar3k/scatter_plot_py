c49460540ecae24fe1e617626dc8b4798a57c570
czar3k <czar3k@andraka.eu>
2020-06-17 01:04:22 +0200

    drawing seaborn swarmplot and new example csv file

--- scatter_plot.py
+++ scatter_plot.py
@@ -1,6 +1,12 @@
 import pandas as pd
+import matplotlib.pyplot as plt
+plt.style.use('seaborn-whitegrid')
+import numpy as np
+import seaborn as sns
+sns.set(style="whitegrid")
 
-file = 'data/RT_exp vs ctrl_avoidance.csv'
+#file = 'data/RT_exp vs ctrl_avoidance.csv'
+file = 'avoidance_ch.csv_example'
 
 columns = pd.read_csv(file, sep=';', nrows=0).columns
 ccc = pd.read_csv(file, sep=';', header=None)
@@ -16,7 +22,7 @@ print(df.iat[1,1])
 
 print(len(df.columns))
 print(len(df))
-print('#######  BRAKE ######\n')
+print('#######  BRAKE #######\n')
 
 tabcol1 = []
 tabcol2 = []
@@ -33,5 +39,36 @@ for i in range(0,len(df.columns)):
         tabnr += 1
     
 
-print('tabcol1: ', tabcol1)
-print('tabcol2: ', tabcol2)
+
+
+if not tabcol2:
+    print('tabcol1: ', tabcol1)
+    df1 = pd.read_csv(file, sep=';',converters=f, usecols=tabcol1)
+    print(df1.head(5))
+else:
+    print('tabcol1: ', tabcol1)
+    df1 = pd.read_csv(file, sep=';',converters=f, usecols=tabcol1)
+    print(df1.head(5))
+    print('tabcol2: ', tabcol2)
+    df2 = pd.read_csv(file, sep=';',converters=f, usecols=tabcol2)
+    print(df2.head(5))
+
+'''
+x=[]
+y=[]
+for i in range(1,len(df)):
+    cn = ccc.iat[i,0]
+    if '*' not in str(cn) and 'nan' not in str(cn):
+        x.append(3)
+        y.append(float(cn.replace(',', '.')))
+print(x)
+print(y)
+plt.scatter(x, y)
+plt.show()
+'''
+#beh = sns.load_dataset(df)
+
+sns.swarmplot(x='type', y='value', data=df)
+
+#print(type(df))
+plt.show()
