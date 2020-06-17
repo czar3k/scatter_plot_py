ba744fa980d121006c936ce7e4142bb80e5efe68
czar3k <czar3k@andraka.eu>
2020-06-17 17:50:57 +0200

    new file

---
+++ plot_gen.py
@@ -0,0 +1,10 @@
+import pandas as pd
+import seaborn as sns
+sns.set(style="whitegrid")
+import matplotlib.pyplot as plt
+plt.style.use('seaborn-whitegrid')
+import numpy as np
+from numpy.random import seed
+from numpy.random import randn
+from scipy.stats import shapiro
+
--- scatter_plot.py
+++ scatter_plot.py
@@ -1,24 +1,31 @@
 import pandas as pd
+import seaborn as sns
+sns.set(style="whitegrid")
 import matplotlib.pyplot as plt
 plt.style.use('seaborn-whitegrid')
 import numpy as np
-import seaborn as sns
-sns.set(style="whitegrid")
+from numpy.random import seed
+from numpy.random import randn
+from scipy.stats import shapiro
+
 
 #file = 'data/RT_exp vs ctrl_avoidance.csv'
-file = 'avoidance_ch.csv_example'
+file = 'data/avoidance_h.csv'
+#file = 'avoidance_ch.csv_example'
 
 columns = pd.read_csv(file, sep=';', nrows=0).columns
 ccc = pd.read_csv(file, sep=';', header=None)
 
 f = {col : lambda x : (x.replace(",", ".")) for col in columns}
 df = pd.read_csv(file, sep=';',converters=f)
+#df = df.astype({'value':float, 'type':str})
 
 print(df.head(5))
 #print(df['ID_ch'][0]+df['RD_ch'][1])
 #print(df['RD_ch'].dtypes)
-print(df.iat[1,1])
+print(df.iat[0,0]+df.iat[1,0])
 
+print(type(df.iat[0,0]))
 
 print(len(df.columns))
 print(len(df))
@@ -39,12 +46,15 @@ for i in range(0,len(df.columns)):
         tabnr += 1
     
 
-
-
 if not tabcol2:
     print('tabcol1: ', tabcol1)
     df1 = pd.read_csv(file, sep=';',converters=f, usecols=tabcol1)
     print(df1.head(5))
+    col1 = []
+    for i in range(0,len(df)):
+        col1.append(df1.iat[i,0])
+        #print(df1.iat[i,0])
+    print(col1)
 else:
     print('tabcol1: ', tabcol1)
     df1 = pd.read_csv(file, sep=';',converters=f, usecols=tabcol1)
@@ -67,8 +77,14 @@ plt.scatter(x, y)
 plt.show()
 '''
 #beh = sns.load_dataset(df)
-
-sns.swarmplot(x='type', y='value', data=df)
+'''
+sns.boxplot(x="type", y="value", data=df, whis=np.inf)
+sns.swarmplot(x='type', y='value', data=df, size=9, color='.2')
+#sns.boxplot(x=df['type'], y=df['value'])
 
 #print(type(df))
 plt.show()
+'''
+#print(np.inf)
+
+
